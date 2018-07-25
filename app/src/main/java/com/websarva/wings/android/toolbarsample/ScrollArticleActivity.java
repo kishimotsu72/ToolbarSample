534959b55f5f89c8dd2ec238c9b3ce1450bd7179
kishimotsu72 <kishimotsu72@gmail.com>
2018-07-25 10:31:03 +0900

    ツールバーの設定

--- app/src/main/java/com/websarva/wings/android/toolbarsample/ScrollArticleActivity.java
+++ app/src/main/java/com/websarva/wings/android/toolbarsample/ScrollArticleActivity.java
@@ -1,7 +1,9 @@
 package com.websarva.wings.android.toolbarsample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ScrollArticleActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class ScrollArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_article);
+
+
+        //Toolbarを取得。
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //ツールバーにロゴを設定。
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        //ツールバーにタイトル文字列を設定。
+        toolbar.setTitle(R.string.toolbar_title);
+        //ツールバーのタイトル文字色を設定。
+        toolbar.setTitleTextColor(Color.WHITE);
+        //ツールバーのサブタイトル文字列を設定。
+        toolbar.setSubtitle(R.string.toolbar_subtitle);
+        //ツールバーのサブタイトル文字色を設定。
+        toolbar.setSubtitleTextColor(Color.LTGRAY);
+        //アクションバーにツールバーを設定。
+        setSupportActionBar(toolbar);
     }
 }
